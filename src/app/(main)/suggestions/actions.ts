fbf8238180988f9b800272bfdb11669349a6ed37
corentindautreme <co@Cos.localdomain>
2025-06-08 21:50:56 +0200

    Prevent DynamoDB requests when on debug mode

--- src/app/(main)/suggestions/actions.ts
+++ src/app/(main)/suggestions/actions.ts
@@ -18,9 +18,13 @@ export async function submitSuggestions(suggestions: Suggestion[]): Promise<Data
                 const {key: _, ...event} = e;
                 return event;
             });
-        await publishProcessedSuggestions(processedSuggestions);
-        await publishEventChanges(events, [], []);
-        console.log(`Updated ${processedSuggestions.length} suggestions and saved ${events.length} events to AWS`);
+        if (process.env.DEBUG === "TRUE") {
+            console.log(`Debug is TRUE - did not update ${processedSuggestions.length} suggestionsnor save ${events.length} events on AWS`);
+        } else {
+            await publishProcessedSuggestions(processedSuggestions);
+            await publishEventChanges(events, [], []);
+            console.log(`Updated ${processedSuggestions.length} suggestions and saved ${events.length} events to AWS`);
+        }
         return {success: true};
     } catch (error) {
         if (error instanceof Error) {
